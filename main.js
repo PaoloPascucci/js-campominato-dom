94ba96e09f98a04032605a805722307e3bdf11a6
PaoloPascucci <pascuccip9@gmail.com>
2021-10-21 16:52:33 +0200

    Add bombs(problem)

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 */
-let UserTrial = prompt('Scegli la difficoltà: Facile, Medio o Difficile?')
+const UserTrial = prompt('Scegli la difficoltà: Facile, Medio o Difficile?')
+
+// let NCell ;
 
 if (UserTrial == "Facile") {
     easy()
@@ -22,8 +24,6 @@ else {
 }
 
 
-
-
 function easy() {
     const cont = document.getElementById('container');
     cont.classList.add("easy");
@@ -36,6 +36,9 @@ function easy() {
         Cell.addEventListener('click', function () {
             this.style.backgroundColor = "skyblue";
             // console.log('cliccato');
+
+            const Bombe = Genbombs(100);
+            console.log(Bombe);
         })
     }
 }
@@ -51,6 +54,8 @@ function medium() {
         Cell.addEventListener('click', function () {
             this.style.backgroundColor = "skyblue";
             // console.log('cliccato');
+            const Bombe = Genbombs(81);
+            console.log(Bombe);
         })
     }
 }
@@ -62,9 +67,50 @@ function hard() {
         Cell.classList.add("gridcell");
         cont.append(Cell);
         Cell.innerHTML += i;
+        // const Bombe = Genbombs();
+        // console.log(Bombe);
         Cell.addEventListener('click', function () {
             this.style.backgroundColor = "skyblue";
             // console.log('cliccato');
+
         })
     }
 }
+
+/**
+ Consegna
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+I numeri nella lista delle bombe non possono essere duplicati.
+In seguito l'utente clicca su ogni cella:
+se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
+Al termine della partita il software deve scoprire tutte le bombe e comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+ */
+//generate random bombs
+function Genbombs() {
+    const bombs = [];
+    for (i = 0; i < 16; i++) {
+        const RandomNumber = Rnumber(1, 100)
+        if (!bombs.includes(RandomNumber)) {
+            bombs.push(RandomNumber)
+        }
+    }
+    //return bombs
+    console.log(bombs);
+}
+
+
+//generate random number
+function Rnumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// //function is_a_bomb(Bombe) {
+//     if (bombs.includes(NCell)) {
+//         this.style.backgroundColor = "crimson";
+//     } else {
+//         this.style.backgroundColor = "skyblue";
+//     }
+// }
+
+
